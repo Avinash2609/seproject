2504c64e3a0957028d425a89c86bbe29e9c2835a
Avinash Jindal <ajavinash63@gmail.com>
2020-07-29 11:55:25 +0530

    done

--- app.js
+++ app.js
@@ -16,17 +16,7 @@ var bodyparser=require("body-parser");
 app.use(bodyparser.urlencoded({extended:true}));
 
 var mongoose=require("mongoose");
-// mongoose.connect("mongo "mongodb+srv://cluster0.qa8fk.mongodb.net/YelpCamp" --username Avinash2609");
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://Avinash2609:Avinash@5181@cluster0.qa8fk.mongodb.net/YelpCamp?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
+mongoose.connect("mongodb+srv://Avinash2609:Avinash@5181@cluster0.qa8fk.mongodb.net/YelpCamp?retryWrites=true&w=majority");
 
 var flash = require("connect-flash");
 app.use(flash());
